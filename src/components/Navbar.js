a1a74040302ee6812ee9d0d4443512b8ade92dbe
Akshay0777 <41819302+Akshay0777@users.noreply.github.com>
2021-10-10 17:17:00 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,14 @@ export default function Navbar(props) {
             <div className="container-fluid">
             <a className="navbar-brand" href="#">{props.title}</a>
             <div className=" navbar">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+                    {/* <li class="nav-item mr-5">
+                        <a class="nav-link" href="https://github.com/Akshay0777/textutils" target="_blank"><i class="fab fa-github">Source</i></a>
+                    </li> */}
                 </ul>
+                <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark':'light'}`}>
+                <a class="nav-link" href="https://github.com/Akshay0777/textutils" target="_blank"><i class="fab fa-github">Source</i></a>
+                </div>
                 <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark':'light'}`}>
                     <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
                     <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
